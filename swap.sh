612b251af6fb167670dfa6031e4f79a1277f83f5
Hizham-ui <hizhamxo@gmail.com>
2026-09-11 12:13:21 +0530

    Update swap.sh

--- swap.sh
+++ swap.sh
@@ -2,10 +2,28 @@
 
 # =====================================
 # Swap Space Creation Script
-# Student Name:
-# Roll Number:
+# Student Name: HISHAM
+# Roll Number:1U24IT044
 # =====================================
-
 # Write your commands below
 
+# Disable existing swap
+sudo swapoff /swapfile
 
+# Remove existing swapfile if present
+sudo rm -f /swapfile
+
+# Create a 1GB swap file using dd
+sudo dd if=/dev/zero of=/swapfile bs=1M count=1024
+
+# Set proper permissions (readable/writable by root only)
+sudo chmod 600 /swapfile
+
+# Create swap signature on the file
+sudo mkswap /swapfile
+
+# Activate the swap space
+sudo swapon /swapfile
+
+# Display the activated swap space
+sudo swapon --show
